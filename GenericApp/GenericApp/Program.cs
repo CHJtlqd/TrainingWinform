fc338207bf715be29bbffbb8f4a4ab97cbe5912c
CHJtlqd <tlqd123@outlook.com>
2020-06-15 12:20:09 +0900

    첫 커밋

---
+++ GenericApp/GenericApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApp
+{
+    public class SimpleGeneric<T>
+    {
+        private T[] values;
+        private int index;
+        public SimpleGeneric(int len)   // Constructor
+        {
+            values = new T[len];
+            index = 0;
+        }
+
+        public void Add(params T[] args)
+        {
+            foreach (T e in args)
+                values[index++] = e;
+        }
+
+        public void Print()
+        {
+            foreach (T e in values)
+                Console.Write(e + " ");
+            Console.WriteLine();
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
